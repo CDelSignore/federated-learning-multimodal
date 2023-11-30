2afd3c789f921811d04b28fa64fd95fa50d653b9
Collin DelSignore <collindelsignore@outlook.com>
2023-11-30 11:06:31 -0500

    Focus on single config for debugging

--- src/main.py
+++ src/main.py
@@ -12,10 +12,16 @@ def read_config(path):
 
     return config
 
-data_configs = [os.path.abspath(os.path.join(root, name)) for root, dirs, files in os.walk("../config") for name in files]
+data_configs = [os.path.abspath(os.path.join(root, name)) for root, dirs, files in os.walk("./config") for name in files]
 
-for path in data_configs:
-    print(path)
-    config = read_config(path)
-    fl = FL(config)
-    fl.start()
+#for path in data_configs:
+#    print(path)
+#    config = read_config(path)
+#    fl = FL(config)
+#    fl.start()
+
+path = data_configs[0]
+print(path)
+config = read_config(path)
+fl = FL(config)
+fl.start()
