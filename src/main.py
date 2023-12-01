60d2dee4584bf29088a48c77e2a83d69f5c83203
Collin DelSignore <cdelsignore@umass.edu>
2023-12-01 18:46:10 -0500

    Restore main function to original state where it runs all available configs (testing complete)

--- src/main.py
+++ src/main.py
@@ -14,14 +14,8 @@ def read_config(path):
 
 data_configs = [os.path.abspath(os.path.join(root, name)) for root, dirs, files in os.walk("./config") for name in files]
 
-#for path in data_configs:
-#    print(path)
-#    config = read_config(path)
-#    fl = FL(config)
-#    fl.start()
-
-path = data_configs[0]
-print(path)
-config = read_config(path)
-fl = FL(config)
-fl.start()
+for path in data_configs:
+    print(path)
+    config = read_config(path)
+    fl = FL(config)
+    fl.start()
